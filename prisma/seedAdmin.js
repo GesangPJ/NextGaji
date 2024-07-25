0321ccb1860b50e98f0c3b013ac3157116338159
Gesang Paudra J <g.paudra@gmail.com>
2024-07-25 19:36:46 +0700

    Update seedAdmin.js

--- prisma/seedAdmin.js
+++ prisma/seedAdmin.js
@@ -9,6 +9,15 @@ async function main() {
   const password = 'admin0123'
   const name = 'Admin 0'
   const userType = 'ADMIN'
+  const alamat = "BSD City Zora B1 no.1"
+  const telepon = 628123456789
+  const jeniskelamin = 'PRIA'
+  const tanggungan = 3
+  const gajipokok = 25000000
+  const status_akun = 'AKTIF'
+  const nik = 53274507000099
+  const npwp = 5552432335235
+  const jabatan = 'pemilik'
 
   // Hash the password
   const hashedPassword = await bcrypt.hash(password, 12)
@@ -21,10 +30,19 @@ async function main() {
         password: hashedPassword,
         name,
         userType,
+        alamat,
+        telepon,
+        jeniskelamin,
+        gajipokok,
+        tanggungan,
+        status_akun,
+        nik,
+        npwp,
+        jabatan,
       },
     })
 
-    console.log('Admin user created:', user)
+    console.log('Admin created:', user)
   } catch (error) {
     console.error('Error creating user:', error)
   } finally {
