58ec7bab7d4894b52410eb2b7faca18ca037a5ed
Gesang Paudra J <g.paudra@gmail.com>
2024-07-26 16:12:45 +0700

    edit halaman pph21

--- src/app/dashboard/pph21/page.jsx
+++ src/app/dashboard/pph21/page.jsx
@@ -1,10 +1,40 @@
+'use client'
 
+import { useEffect } from 'react'
 
+import { useRouter } from 'next/navigation'
+
+import { useSession } from 'next-auth/react'
+
+import FormDataPph21 from '@/views/form-pph21/FormPph21'
 
 const HalamanDataPph21 = () =>{
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loading') return // Jangan lakukan apa pun saat sesi sedang dimuat
+
+    if (!session) {
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if (!session) {
+    return null
+  }
 
   return(
     <div>
+      <div>
+        <h1 className='text-2xl font-bold'>
+          Data Nilai Pph21
+        </h1>
+      </div>
+      <br />
+      <div>
+        <FormDataPph21/>
+      </div>
 
     </div>
   )
