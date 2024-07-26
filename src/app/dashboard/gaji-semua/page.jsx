c81a1d9b27d8b7fd6594d8d6ae7b6a11c12e8ff1
Gesang Paudra J <g.paudra@gmail.com>
2024-07-26 10:13:36 +0700

    buat halaman slip gaji seluruh karyawan

---
+++ src/app/dashboard/gaji-semua/page.jsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { useSession } from 'next-auth/react'
+
+
+const GajiSeluruh = () =>{
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loading') return // Jangan lakukan apa pun saat sesi sedang dimuat
+
+    if (!session) {
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if (!session) {
+    return null
+  }
+
+  return(
+    <div>
+      <div>
+        <h1 className="text-xl font-bold">
+          Slip Gaji Semua Karyawan
+        </h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default GajiSeluruh
